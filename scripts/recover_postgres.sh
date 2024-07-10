18cb20b4a526d0407c977e6c047ac244ae8083f9
Yidian Sun <yidian.sun@enterprisedb.com>
2024-07-10 20:33:50 +0800

    fix restart pg command

--- scripts/recover_postgres.sh
+++ scripts/recover_postgres.sh
@@ -1,3 +1,3 @@
-sudo rm /var/lib/postgresql/14/main/postgresql.auto.conf
+sudo rm /var/lib/postgresql/13/main/postgresql.auto.conf
 sleep 2
-su - postgres -c '/usr/lib/postgresql/14/bin/pg_ctl restart -D /var/lib/postgresql/14/main/ -o "-c config_file=/etc/postgresql/14/main/postgresql.conf"'
+sudo su - postgres -c '/usr/lib/postgresql/13/bin/pg_ctl restart -D /var/lib/postgresql/13/main/ -o "-c config_file=/etc/postgresql/13/main/postgresql.conf"'
